4d93ee3979c7c1709b92aee1803dd01bfae35ed1
Daniel Bezerra <danielbezerra@poli.ufrj.br>
2025-04-16 19:34:29 -0300

    -Conclusão da lógica de treinamento e previsão dos modelos; -Início do arquivo plotresults, responsável pela criação das janelas de plot e da classe de plotagem, com respectivas métricas, tipos de plot, etc.

--- models.py
+++ models.py
@@ -7,24 +7,30 @@ from darts.models import NBEATSModel
 from darts.models import NHiTSModel
 
 class ModelRunWindow(ctk.CTkToplevel):
-    def __init__(self, params, configs, file=None, series=None, predictions=[]):
+    def __init__(self, params, configs, series_file=None, series=None, predictions=None):
         super().__init__()
         self.grab_set()
         self.grid_propagate(True)
-        self.file = file
-        if file:
-            self.series = GetSeries(file)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)  # Espaço antes dos botões
+        self.rowconfigure(1, weight=1)
+        self.file = series_file
+        if series_file:
+            self.series = GetSeries(series_file)
         else:
             self.series = series
         self.params = params
         self.configurations = configs
-        self.predictions = predictions
+        if predictions:
+            self.predictions = predictions
+        else:
+            self.predictions=[]
         self.model = None
         self.epochs = None
 
         #Barra de Progresso do Treinamento
         self.progress = ctk.CTkProgressBar(self)
-        self.progress.pack(pady=20)
+        self.progress.grid(row=0, column=0, pady=20)
         self.progress.set(0)
 
 
@@ -33,14 +39,28 @@ class ModelRunWindow(ctk.CTkToplevel):
         for epoch in range(self.epochs):
             self.model.fit(series=self.series.train, val_series=self.series.valid)
             self.update_progress(epoch)
-        if self.params['save_checkpoints'] == 'True':
+        if self.params['save_checkpoints'] == 'true':
             self.model = NBEATSModel.load_from_checkpoint(self.model.model_name)
-        print(self.model)
+        self.predict_model()
 
     def update_progress(self,epoch):
         progress_value = (epoch + 1) / self.epochs
         self.progress.set(progress_value)
         self.update_idletasks()
+
+    def next_model_run(self):
+        # Fecha modelo atual e abre próximo
+        if self.configurations:
+            next_model_run = self.configurations[0]
+            self.configurations.pop(0)
+            if next_model_run["model"] == "N-BEATS":
+                ModelRunNBEATSWindow(params=next_model_run["parameters"], configs=self.configurations, series=self.series, preds=self.predictions)
+            if next_model_run["model"] == "N-HiTS":
+                ModelRunNHiTSWindow(params=next_model_run["parameters"], configs=self.configurations, series=self.series, preds=self.predictions)
+            self.destroy()
+        # else:
+        #     ResultsWindow()
+        #     self.after(100, self.destroy)
 
     def centralize_window(self):
         # window_width = round(self.winfo_width(),-1)
@@ -56,67 +76,63 @@ class ModelRunWindow(ctk.CTkToplevel):
     def bring_fwd_window(self):
         self.attributes("-topmost", True)
 
-    def next_model_run(self):
-        # Fecha modelo atual e abre próximo
-        if self.configurations:
-            next_model_run = self.configurations[0]
-            self.configurations.pop(0)
-            if next_model_run["model"] == "N-BEATS":
-                ModelRunNBEATSWindow(next_model_run["parameters"], self.configurations, self.series)
-            if next_model_run["model"] == "N-HiTS":
-                ModelRunNHiTSWindow(next_model_run["parameters"], self.configurations, self.series)
-            self.after(100, self.destroy)
-        # else:
-        #     ResultsWindow()
-        #     self.after(100, self.destroy)
-
 class ModelRunLHCWindow(ModelRunWindow):
-    def __init__(self,params, series_file, configs):
-        super().__init__(params, series_file, configs)
+    def __init__(self,params, configs, series_file=None, series=None, preds=None):
+        super().__init__(params, configs, series_file, series, preds)
         self.title("LHC - Executando Modelo")
-        self.centralize_window()
-        self.bring_fwd_window()
-
-        self.model_creation()
-        self.after(100, self.model_train)
-        self.evaluate_model()
-        self.next_model_run()
-
-    def model_creation(self):
-        self.epochs = self.params["n_epochs"]
-        self.params["n_epochs"] = 1
-        self.model = LHCModel(**self.params)
+    #     self.centralize_window()
+    #     self.bring_fwd_window()
+    #
+    #     self.model_creation()
+    #     self.after(100, self.model_train)
+    #     self.evaluate_model()
+    #     self.next_model_run()
+    #
+    # def model_creation(self):
+    #     self.epochs = self.params["n_epochs"]
+    #     self.params["n_epochs"] = 1
+    #     self.model = LHCModel(**self.params)
 
 class ModelRunNBEATSWindow(ModelRunWindow):
-    def __init__(self,params, series_file, configs):
-        super().__init__(params, series_file, configs)
+    def __init__(self, params, configs, series_file=None, series=None, preds=None):
+        super().__init__(params, configs, series_file, series, preds)
         self.title("N-BEATS - Executando Modelo")
         self.centralize_window()
         self.bring_fwd_window()
 
         self.model_creation()
         self.after(100, self.model_train)
-        self.evaluate_model()
-        self.next_model_run()
 
     def model_creation(self):
         self.epochs = self.params["n_epochs"]
         self.params["n_epochs"] = 1
         self.model = NBEATSModel(**self.params)
 
+    def predict_model(self):
+        prediction= self.model.predict(series=self.series.train, n=len(self.series.valid))
+        print(prediction)
+        self.predictions.append({"NBEATS":prediction})
+        print(self.predictions)
+        self.next_model_run()
+
 class ModelRunNHiTSWindow(ModelRunWindow):
-    def __init__(self,params, series_file, configs):
-        super().__init__(params, series_file, configs)
+    def __init__(self, params, configs, series_file=None, series=None, preds=None):
+        super().__init__(params, configs, series_file, series, preds)
         self.title("N-HiTS - Executando Modelo")
         self.centralize_window()
         self.bring_fwd_window()
 
         self.model_creation()
         self.after(100, self.model_train)
-        self.evaluate_model()
-        self.next_model_run()
 
     def model_creation(self):
         self.epochs = self.params["n_epochs"]
         self.params["n_epochs"] = 1
         self.model = NHiTSModel(**self.params)
+
+    def predict_model(self):
+        prediction= self.model.predict(series=self.series.train, n=len(self.series.valid))
+        print(prediction)
+        self.predictions.append({"NHiTS":prediction})
+        print(self.predictions)
+        self.next_model_run()
