0f31a4b395af447fd11112b52d06a7fac211e48d
Daniel Bezerra <danielbezerra@poli.ufrj.br>
2025-05-15 01:09:57 -0300

    -Corrigindo identação em lhcmodel.

--- lhcmodel.py
+++ lhcmodel.py
@@ -39,6 +39,7 @@ class LHCModel(nn.Module):
         out = self.fc(out)
         return out
 
+
 #APENAS DENTRO DA JANELA QUANDO FOR TREINAR
 
 # model = LHCModel(input_size, hidden_size, num_layers, output_size).to(device)
